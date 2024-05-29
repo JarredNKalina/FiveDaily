b7fc8756ccbefcd07484f9e424b5f69c87dd9512
JarredNKalina <jarredkalina@gmail.com>
2024-05-29 17:35:22 -0400

    Initial Commit

---
+++ FiveDaily/FiveDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveDailyApp.swift
+//  FiveDaily
+//
+//  Created by Jarred Kalina on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FiveDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
